5569534e8b019855cef173e48837c2d536f70287
Егор <egorkrivorotov@mail.ru>
2021-08-04 01:05:59 +0300

    Добавил каунтеры для фильтров

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import {createShowMoreButtonTemplate} from './view/show-more-button.js';
 import {createFooterStatisticsTemplate} from './view/footer-statistics.js';
 import {getGenres, getComments, createGenresTemplate, createCommentsTemplate, createFilmDetailsTemplate} from './view/film-details.js';
 import {generateFilm} from './mock/film.js';
+import {generateFilter} from './mock/filter.js';
+
+const FILM_COUNT_PER_STEP = 5;
 
 const FilmCardCount = {
   ALL: 20,
@@ -22,6 +25,7 @@ const RenderPosition = {
 };
 
 const films = new Array(FilmCardCount.ALL).fill().map(generateFilm);
+const filters = generateFilter(films);
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -33,14 +37,14 @@ const mainElement = document.querySelector('.main');
 const footerStatistics = document.querySelector('.footer__statistics');
 
 render(headerElement, createHeaderProfileTemplate(), RenderPosition.BEFORE_END);
-render(mainElement, createMenuTemplate(), RenderPosition.BEFORE_END);
+render(mainElement, createMenuTemplate(filters), RenderPosition.BEFORE_END);
 render(mainElement, createSortTemplate(), RenderPosition.BEFORE_END);
 render(mainElement, createFilmsListTemplate(), RenderPosition.BEFORE_END);
 
 const filmsList = mainElement.querySelector('.films-list');
 const filmsContainers = mainElement.querySelectorAll('.films-list__container');
 
-for (let i = 0; i < FilmCardCount.ALL; i++) {
+for (let i = 0; i < Math.min(films.length, FILM_COUNT_PER_STEP); i++) {
   render(filmsContainers[0], createFilmCardTemplate(films[i]), RenderPosition.BEFORE_END);
 }
 
@@ -52,8 +56,7 @@ for (let i = 0; i < FilmCardCount.COMMENTED; i++) {
   render(filmsContainers[2], createFilmCardTemplate(films[i]), RenderPosition.BEFORE_END);
 }
 
-render(filmsList, createShowMoreButtonTemplate(), RenderPosition.BEFORE_END);
-render(footerStatistics, createFooterStatisticsTemplate(), RenderPosition.BEFORE_END);
+render(footerStatistics, createFooterStatisticsTemplate(films), RenderPosition.BEFORE_END);
 render(body, createFilmDetailsTemplate(films[0]), RenderPosition.BEFORE_END);
 
 const filmTableCells = document.querySelectorAll('.film-details__cell');
@@ -67,8 +70,27 @@ for (let i = 0; i < genres.length; i++) {
 const commentsList = document.querySelector('.film-details__comments-list');
 
 const comments = getComments();
-console.log(comments);
 
 for (let i = 0; i < comments.length; i++) {
   render(commentsList, createCommentsTemplate(comments[i]), RenderPosition.BEFORE_END);
+}
+
+if (films.length > FILM_COUNT_PER_STEP) {
+  let renderedFilmCount = FILM_COUNT_PER_STEP;
+
+  render(filmsList, createShowMoreButtonTemplate(), RenderPosition.BEFORE_END);
+
+  const showMoreButton = filmsList.querySelector('.films-list__show-more');
+
+  showMoreButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    films.slice(renderedFilmCount, renderedFilmCount + FILM_COUNT_PER_STEP).forEach((film) => render(filmsContainers[0], createFilmCardTemplate(film), RenderPosition.BEFORE_END));
+
+    renderedFilmCount += FILM_COUNT_PER_STEP;
+
+    if (renderedFilmCount >= films.length) {
+      showMoreButton.remove();
+    }
+  });
 }
--- src/view/menu.js
+++ src/view/menu.js
@@ -1,10 +1,10 @@
-const createMenuTemplate = () => (
+const createMenuTemplate = (filters) => (
   `<nav class="main-navigation">
     <div class="main-navigation__items">
       <a href="#all" class="main-navigation__item">All movies</a>
-      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${filters[1].count}</span></a>
+      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${filters[2].count}</span></a>
+      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${filters[3].count}</span></a>
     </div>
     <a href="#stats" class="main-navigation__additional main-navigation__additional--active">Stats</a>
   </nav>`
